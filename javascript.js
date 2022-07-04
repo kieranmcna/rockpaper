1ced45726c8c229d1724f2cfd4ee50df3473637b
kierkier27 <kieranmcnamara27@gmail.com>
2022-07-04 18:05:38 +0100

    add javascript.js

---
+++ javascript.js
@@ -0,0 +1,36 @@
+let rock;
+let paper;
+let Scissors;
+
+let playerAnswer=prompt("Enter Your Answer:");
+switch(playerAnswer_{
+    case "rock":
+    text = "Rock beats blah blah blah";
+    break;
+    case "paper":
+    
+}
+
+
+function computerPlay(){
+        let values = ["rock","scissors","paper"],
+        valueToUse = values[Math.floor(Math.random() * values.length)];
+        // do something with the selected value
+}
+function playRound(playerSelection, computerSelection)
+
+
+function playerSelection()
+
+
+function playerSelection
+Input text 
+rock paper Scissors
+
+
+rock beats Scissors
+scissors beats paper
+paper beats rock
+
+
+
